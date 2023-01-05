7a7386d69cb37cf68c8e1f5d1569da16dce93fc8
aakter3 <121882119+aakter3@users.noreply.github.com>
2023-01-05 12:10:11 -0500

    Add files via upload

---
+++ ex11.js
@@ -0,0 +1,38 @@
+let college = 'York College';
+const myName = 'Jane Doe';
+
+console.log(college);
+console.log(myName);
+
+const favoriteSubject = 'Math';
+//Console.log the text "My name is: " follows by myName variable.
+console.log("My name is:", myName, ", I go to ", college);
+console.log("My name is:" + myName);
+//Console.log the text "My college is: " follows by college variable.
+console.log("My College is:" + college);
+//Reassign the value stored in favoriteSubject to the string "Coding", is it possible?
+// Ans: NO
+
+//can you console.log(number1) here?
+//console.log(number1); give me undefined.
+
+//Declare a new variable named 'number1' and give it the value of 10.
+
+let number1 = 10;
+//Declare a new variable named 'number2' and give it the value of 15.
+let number2 = 15;
+
+//console.log the summation of number1 and number2 in the following format
+console.log("The sum is:" + (number1+number2));
+//Expected output: The sum is 25
+// The sum is:25
+//console.log the product of number1 and number2 in the following format
+console.log("The sum is:" + (number1*number2));
+//Expected output: The product is 150
+
+//Declare a new variable named 'check' and give it the value of true (Boolean).
+let check = true;
+console.log(check);
+document.getElementById('demo').innerText="The sum is "+(number1 + number2);
+//console.log check and see the value, try also with false.
+
